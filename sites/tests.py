d2df5a307665fb3d14ef0eacbef1b15042976b3a
Santiago Rendon Valencia <srendonv@unal.edu.co>
2015-02-26 01:29:29 +0100

    final

--- sites/tests.py
+++ sites/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.core.urlresolvers import reverse
+
+class SiteViewTests(TestCase):
+    def test_index_view_with_no_sites(self):
+        """
+        If no sites exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('sites:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No sites are available.")
--- sites/views.py
+++ sites/views.py
@@ -3,18 +3,12 @@ from sites.models import Site
 from sites.models import Value
 from django.http import Http404
 from django.db.models import Sum, Avg
-
-def avg(list):
-
-    sum = 0
-    for elm in list:
-        sum += elm
-
-    sum/(len(list)*1.0)
+import itertools
+import operator
     
 def index(request):
     all_sites = Site.objects.all();
-    return render(request,'sites/index.html',{'all_sites':all_sites,'title':'Sities'})
+    return render(request, 'sites/index.html', {'all_sites':all_sites, 'title':'Sities'})
     
 
 def detail(request, site_id):
@@ -23,14 +17,25 @@ def detail(request, site_id):
         values = site.value_set.all()
     except Site.DoesNotExist:
         raise Http404("Site does not exist")
-    return render(request,'sites/detail.html',{'values':values,'site':site})
+    return render(request, 'sites/detail.html', {'values':values, 'site':site})
 
-def summary(request):
-    values = Value.objects.values("site__name").annotate(a_value=Sum('a_value')).annotate(b_value =Sum('b_value'))
-    return render(request,'sites/summary.html',{'values':values,'title':'Summary','aggre':'sum'})
+def accumulate(l):
+    it = itertools.groupby(l, operator.itemgetter(0))
+    list = []
+    for key, subiter in it:
+        sum_a, sum_b = [], []
+        for n in subiter:
+            sum_a.append(n[1])
+            sum_b.append(n[2])
+        list.append({'site__name':key, 'a_value':sum(sum_a), 'b_value':sum(sum_b)})
+    return list
+
+def summary(request):  
+    v = Value.objects.values_list('site__name', 'a_value', 'b_value')
+    values = list(accumulate(v))
+#     values = Value.objects.values("site__name").annotate(a_value=Sum('a_value')).annotate(b_value =Sum('b_value'))
+    return render(request, 'sites/summary.html', {'values':values, 'title':'Summary', 'aggre':'sum'})
 
 def average(request):
-#     list = [3,4,1,20,102,3,5,67,39,28,10,1,4,34,1,6,107,99]
-    values = Value.objects.values("site__name").annotate(a_value=Avg('a_value')).annotate(b_value =Avg('b_value'))
-
-    return render(request,'sites/summary.html',{'values':values,'title':'Summary','aggre':'avg'})
+    values = Value.objects.values("site__name").annotate(a_value=Avg('a_value')).annotate(b_value=Avg('b_value'))
+    return render(request, 'sites/summary.html', {'values':values, 'title':'Summary', 'aggre':'avg'})
